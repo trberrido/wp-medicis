02bb03e1718e93dc2785dae2a0a433194865d4e4
Thibault Berrido <thibault.berrido@gmail.com>
2025-06-03 22:36:30 +0000

    add: debug fn

---
+++ inc/debug.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+function console(): void {
+
+	$fn_argv = func_get_args();
+
+	echo '<pre style="background-color:#ececec; color: black; padding: 1rem; border: 1px solid #666666; font-size: .8rem; border-radius: .5rem;">';
+	foreach ( $fn_argv as $fn_arg ) {
+		var_dump( $fn_arg );
+		echo '-----------<br>';
+	}
+	echo '</pre>';
+
+}
