cde29079a810760e6eecc68de3d8462de5f5108a
Thibault Berrido <thibault.berrido@gmail.com>
2025-06-03 22:05:00 +0000

    add: activating two menus

---
+++ inc/menu.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action( 'init', 'pm__register_menu' );
+function pm__register_menu() {
+	register_nav_menus( array(
+		'primary'	=> 'Navigation principale',
+		'secondary'	=> 'Navigation secondaire',
+		)
+	);
+}
