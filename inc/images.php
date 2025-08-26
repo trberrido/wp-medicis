47cca1a69da85cdd48042c9e381e288ff486015a
Thibault Berrido <thibault.berrido@gmail.com>
2025-08-26 07:32:59 +0000

    fix: image upload

--- inc/images.php
+++ inc/images.php
@@ -1,7 +1,7 @@
 <?php defined('ABSPATH') or die();
 
 add_filter( 'image_editor_output_format', 'pm__filter_image_editor_output_format' );
-function df__filter_image_editor_output_format( array $formats ): array {
+function pm__filter_image_editor_output_format( array $formats ): array {
 	$formats['image/jpeg'] = 'image/avif';
 	$formats['image/png'] = 'image/avif';
 	/*
