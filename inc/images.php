d764516b663f51dae744311c4434ecbda7e288ec
Thibault Berrido <thibault.berrido@gmail.com>
2025-09-03 18:45:34 +0000

    fix: png > avif

--- inc/images.php
+++ inc/images.php
@@ -10,3 +10,43 @@ function pm__filter_image_editor_output_format( array $formats ): array {
 	 */
 	return $formats;
 }
+
+
+// WordPress bug when converting .png to .avif
+// see https://github.com/WordPress/performance/issues/2018
+add_filter( 'wp_handle_upload_prefilter', 'convert_palette_png_to_truecolor' );
+function convert_palette_png_to_truecolor( $file ) {
+	if ( ! isset( $file['tmp_name'], $file['name'] ) ) {
+		return $file;
+	}
+
+	$ext = strtolower( pathinfo( $file['name'], PATHINFO_EXTENSION ) );
+
+	if ( 'png' !== $ext ) {
+		return $file;
+	}
+
+	$editor = wp_get_image_editor( $file['tmp_name'] );
+
+	if ( is_wp_error( $editor ) || ! $editor instanceof WP_Image_Editor_GD ) {
+		return $file; // Skip conversion if not using GD.
+	}
+
+	$image = imagecreatefrompng( $file['tmp_name'] );
+	if ( ! $image || imageistruecolor( $image ) ) {
+		return $file;
+	}
+
+	// Preserve transparency.
+	imagealphablending( $image, false );
+	imagesavealpha( $image, true );
+
+	// Convert the palette to truecolor.
+	imagepalettetotruecolor( $image );
+
+	// Overwrite the upload with the new truecolor PNG.
+	imagepng( $image, $file['tmp_name'] );
+	imagedestroy( $image );
+
+	return $file;
+}
