5ed73aacdbb1382215662df7c81a568ca2cc58d3
Thibault Berrido <thibault.berrido@gmail.com>
2025-09-11 05:53:28 +0000

    improve pm year

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', e => {
 	console.log('Focus on the donut, not the hole.');
 	document.querySelectorAll('.pm-year-btn').forEach(btn => {
 		btn.addEventListener('click', e => {
+			e.currentTarget.classList.toggle('btn--open');
 			e.currentTarget.closest('.pm-post').querySelector('.wp-block-pm-year-filter').classList.toggle('pm-hidden');
 			e.currentTarget.closest('.wp-block-pm-year-filter').classList.toggle('pm-hidden');
 		});
