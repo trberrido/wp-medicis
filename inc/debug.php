a4e91098fcf0644b11c7f3d5efed73eecf6bc0b0
Thibault Berrido <thibault.berrido@gmail.com>
2025-10-24 05:05:49 +0000

    fix: search filter

--- inc/debug.php
+++ inc/debug.php
@@ -4,7 +4,7 @@ function console(): void {
 
 	$fn_argv = func_get_args();
 
-	echo '<pre style="background-color:#ececec; color: black; padding: 1rem; border: 1px solid #666666; font-size: .8rem; border-radius: .5rem;">';
+	echo '<pre style="position: relative; z-index: 100; background-color:#ececec; color: black; padding: 1rem; border: 1px solid #666666; font-size: .8rem; border-radius: .5rem;">';
 	foreach ( $fn_argv as $fn_arg ) {
 		var_dump( $fn_arg );
 		echo '-----------<br>';
